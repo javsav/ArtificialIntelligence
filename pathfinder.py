4799be7be849b7f6367716ae95a16c300d40599f
javsav <javed.alam@student.adelaide.edu.au>
2026-03-25 23:00:31 +1030

    updated map parsing logic - change to np array of type int. adjusted type of start, size and end tuples to int, and started breadth first search algorithm

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,7 @@
 import sys
+from collections import deque
 
-import numpy
+import numpy as np
 
 if len(sys.argv) < 5:
     print("too few arguments")
@@ -17,6 +18,21 @@ def parse_mode(arg1, arg2, arg3, arg4):
             return print(
                 "Invalid argument - please pass debug or release as the first command"
             )
+
+
+def breadth_first(map, size, start, end):
+    to_visit = deque()
+    visited = np.ones((size[0], size[1]), dtype=bool)
+    for i in range(0, size[0], 1):
+        for j in range(0, size[1], 1):
+            visited[i][j] = False
+    print(visited)
+    closed = np.ones((size[0], size[1]), dtype=bool)
+    adjustRow = {-1, 0, 1, 0}
+    adjustCol = {0, 1, 0, -1}
+
+    visited[start[0], start[1]] = True
+    # if start == (1,1) or size
 
 
 def parse_map():
@@ -37,31 +53,41 @@ def parse_map():
     if len(size_string_split) < 2:
         print("Error:, map size must have 2 coordinates")
         sys.exit()
-    size = (size_string_split[0], size_string_split[1])
+    size = (int(size_string_split[0]), int(size_string_split[1]))
 
     start_string_split = start_string.split()
     if len(start_string_split) < 2:
         print("Error:, start position must have 2 coordinates")
         sys.exit()
-    start = (start_string_split[0], start_string_split[1])
+    start = (int(start_string_split[0]), int(start_string_split[1]))
 
     end_string_split = end_string.split()
     if len(end_string_split) < 2:
         print("Error:, end position must have 2 coordinates")
         sys.exit()
-    end = (end_string_split[0], end_string_split[1])
+    end = (int(end_string_split[0]), int(end_string_split[1]))
 
     remaining_lines = lines[3:]
     map_array = [line.split() for line in remaining_lines]
-    return size, start, end, map_array
+    map_array_int = np.zeros((size[0], size[1]), dtype=int)
+    for i in range(0, size[0], 1):
+        for j in range(0, size[1], 1):
+            if map_array[i][j] == "X":
+                map_array_int[i][j] = 0
+            else:
+                map_array_int[i][j] = int(map_array[i][j])
+
+    return size, start, end, map_array_int
 
 
-def pathfind(mode, map, algorithm, heuristic="euclidian"):
+def pathfind(mode, map_file, algorithm, heuristic="euclidian"):
     if mode == "DEBUG":
         print("debug mode")
-        parse_map()
+        size, start, end, map = parse_map()
+        print(map)
+        breadth_first(map, size, start, end)
     else:
-        parse_map()
+        size, start, end, map = parse_map()
 
 
 parse_mode(sys.argv[1].upper(), sys.argv[2], sys.argv[3].upper(), sys.argv[4].upper())
